451ebfb8a89dfb5a5227c289ece340489c5d282f
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2014-01-16 01:14:16 -0300

    incorporated the use of redis-cloud option

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+import os
+
+# Put here required packages
+packages = ['Django<=1.6',]
+
+if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
+     packages.append('django-redis-cache')
+     packages.append('hiredis')
+
 setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='https://pypi.python.org/pypi',
-      install_requires=['Django<=1.6'],
-     )
+      install_requires=packages,
+)
+
